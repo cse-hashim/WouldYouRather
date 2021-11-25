bddfe77c1b134f1891edab3442c862a92ad9fb06
Hashim Hossam Mahmoud Mohammed <cse-hashim@users.noreply.github.com>
2021-11-25 10:45:15 +0200

    es6

--- store-factory-redux-middleware/index.js
+++ store-factory-redux-middleware/index.js
@@ -1,37 +1,3 @@
-//library code
-// function createStore(reducer) {
-//     //the store should have four parts
-//     //1. the state
-//     //2 get the state
-//     //3 listen to changes on the state
-//     //4 update the state
-//     let state
-//     let listeners = []
-
-//     const getState = () => state
-
-//     const subscribe = (listener) => {
-//         listeners.push(listener)
-//         return () => {
-//             listeners = listeners.filter(l => l !== listener)
-//         }
-//     }
-//     const dispatch = (action) => {
-//         state = reducer(state, action)
-//         listeners.forEach(listener => listener())
-//     }
-
-//     return {
-//         getState,
-//         subscribe,
-//         dispatch
-//     }
-// }
-// const store = createStore()
-
-// store.subscribe(() => { console.log('new state is: ', store.getState()) })
-// store.unsubscribe(() => { console.log('the store changed') })
-
 //app code
 const [
     ADD_GOAL,
@@ -52,7 +18,6 @@ const todos = (state = [], action) => {//this is called a reducer funtion
         case (REMOVE_TODO):
             return state.filter(e => e.id !== action.id)
         case (TOGGLE_TODO):
-            // return state.map(e => e.id !== action.id ? e : Object.assign({}, e, { complete: !e.complete }))
             return state.map(e => e.id !== action.id ? e : { ...e, complete: !e.complete })
         default:
             return state
@@ -69,35 +34,23 @@ const goals = (state = [], action) => {
             return state
     }
 }
-// const app = (state = {}, action) => {
-//     return {
-//         todos: todos(state.todos, action),
-//         goals: goals(state.goals, action),
-//     }
-// }
-// const store = createStore(app)
-
-function checker(store){
-    return function(next){
-        return function(action){
-            if (action.type === ADD_TODO &&
-                action.todo.name.toLowerCase().includes('bitcoin')
-            ) {
-                return alert('Nope. thats a bad idea')
-            }
-            if (action.type === ADD_GOAL &&
-                action.goal.name.toLowerCase().includes('bitcoin')
-            ) {
-                return alert('Nope. thats a bad idea')
-            }
-            return next(action)            
-        }
+const checker = (store) => (next) => (action) => {
+    if (action.type === ADD_TODO &&
+        action.todo.name.toLowerCase().includes('bitcoin')
+    ) {
+        return alert('Nope. thats a bad idea')
     }
+    if (action.type === ADD_GOAL &&
+        action.goal.name.toLowerCase().includes('bitcoin')
+    ) {
+        return alert('Nope. thats a bad idea')
+    }
+    return next(action)
 }
 const store = Redux.createStore(Redux.combineReducers({
     todos,
     goals,
-}),Redux.applyMiddleware(checker))
+}), Redux.applyMiddleware(checker))
 store.subscribe(() => {
     console.log('the new state', store.getState())
 })
